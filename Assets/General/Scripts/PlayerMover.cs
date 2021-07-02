81c8b49846dbfd479611bb791c54f4156a651600
Aynur19 <nasybullin.aynur@yandex.ru>
2021-07-02 12:50:48 +0300

    add gravity velocity

--- Assets/General/Scripts/PlayerMover.cs
+++ Assets/General/Scripts/PlayerMover.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class PlayerMover : MonoBehaviour
 {
 	public CharacterController character;
+	public Transform groundCheck;
+	public float groundDistance = 0.4f;
+	public LayerMask groundMask;
 
 	[SerializeField]
 	private float moveSpeed;
@@ -10,7 +13,12 @@ public class PlayerMover : MonoBehaviour
 	[SerializeField]
 	private float jumpForce;
 
+	[SerializeField]
+	private float gravity = -9.81f;
+
 	private PlayerInput actions;
+	private Vector3 velocity;
+	private bool isGrounded;
 
 	private void Awake()
 	{
@@ -30,8 +38,12 @@ public class PlayerMover : MonoBehaviour
 
 	private void Update()
 	{
+		UpdateVelosity();
+
+
 		var moveDirection = actions.Player.Move.ReadValue<Vector2>();
 		Move(moveDirection);
+
 	}
 
 	private void Move(Vector2 direction)
@@ -42,11 +54,24 @@ public class PlayerMover : MonoBehaviour
 		{
 			var move = (transform.right * direction.x + transform.forward * direction.y) * scaledMoveSpeed;
 			character.Move(move);
+			velocity.y += gravity * Time.deltaTime;
+			character.Move(velocity * Time.deltaTime);
 		}
 		else
 		{
 			var moveDirection = new Vector3(direction.x, 0, direction.y);
 			transform.position += moveDirection * scaledMoveSpeed;
+			velocity.y += gravity * Time.deltaTime;
+			transform.position += velocity * Time.deltaTime;
+		}
+	}
+
+	private void UpdateVelosity()
+	{
+		isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+		if (isGrounded && velocity.y < 0)
+		{
+			velocity.y = -2f;
 		}
 	}
 
