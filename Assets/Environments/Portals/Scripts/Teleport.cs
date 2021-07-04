b3e4b45f27a3a6a83018cec607ae57dba312e42b
Aynur19 <nasybullin.aynur@yandex.ru>
2021-07-04 21:06:12 +0300

    add particle for VFX for portals

--- Assets/Environments/Portals/Scripts/Teleport.cs
+++ Assets/Environments/Portals/Scripts/Teleport.cs
@@ -2,64 +2,27 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
-	public Transform player;
-	public Transform linkedPortal;
+	public Teleport linkedTeleport;
 
-	private bool playerIsOverlapping = false;
-
-	private void Update()
+	private void OnTriggerStay(Collider other)
 	{
-		if (playerIsOverlapping)
+		var zPosition = transform.worldToLocalMatrix.MultiplyPoint3x4(other.transform.position).z;
+
+		if (zPosition < 0)
 		{
-			Debug.Log($"playerIsOverlapping = {playerIsOverlapping}");
-			Vector3 portalToPlayer = player.position - transform.position;
-			float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
-
-			if (dotProduct < 0f)
-			{
-				float rotationDiff = -Quaternion.Angle(transform.rotation, linkedPortal.rotation);
-				rotationDiff += 180;
-				player.Rotate(Vector3.up, rotationDiff);
-
-				Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
-				player.position = linkedPortal.position + positionOffset;
-
-				playerIsOverlapping = false; 
-			}
+			Teleportation(other.transform);
 		}
 	}
 
-	private void OnTriggerEnter(Collider other)
+	private void Teleportation(Transform obj)
 	{
-		if(other.CompareTag("Player"))
-		{
-			Debug.Log($"playerIsOverlapping = {playerIsOverlapping}");
-			playerIsOverlapping = true;
-		}
+		// Position
+		var localPos = transform.worldToLocalMatrix.MultiplyPoint3x4(obj.position);
+		localPos = new Vector3(-localPos.x, localPos.y, -localPos.z);
+		obj.position = linkedTeleport.transform.localToWorldMatrix.MultiplyPoint3x4(localPos);
+
+		// Rotation
+		var difference = linkedTeleport.transform.rotation * Quaternion.Inverse(transform.rotation * Quaternion.Euler(0f, 180f, 0f));
+		obj.rotation = difference * obj.rotation;
 	}
-
-
-	//public Teleport linkedTeleport;
-
-	//private void OnTriggerStay(Collider other)
-	//{
-	//	var zPosition = transform.worldToLocalMatrix.MultiplyPoint3x4(other.transform.position).z;
-
-	//	if (zPosition < 0)
-	//	{
-	//		Teleportation(other.transform);
-	//	}
-	//}
-
-	//private void Teleportation(Transform obj)
-	//{
-	//	// Position
-	//	var localPos = transform.worldToLocalMatrix.MultiplyPoint3x4(obj.position);
-	//	localPos = new Vector3(-localPos.x, localPos.y, -localPos.z);
-	//	obj.position = linkedTeleport.transform.localToWorldMatrix.MultiplyPoint3x4(localPos);
-
-	//	// Rotation
-	//	var difference = linkedTeleport.transform.rotation * Quaternion.Inverse(transform.rotation * Quaternion.Euler(0f, 180f, 0f));
-	//	obj.rotation = difference * obj.rotation;
-	//}
 }
